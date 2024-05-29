aebf8ea3213122f17957ccc6f46e3821205ba859
OparlescuBogdan04 <156832613+OparlescuBogdan04@users.noreply.github.com>
2024-05-29 05:55:31 +0300

    main include path updated

--- include/chat.h
+++ include/chat.h
@@ -1,17 +1,15 @@
-#ifndef CHAT_h
-#define CHAT_h
+#ifndef CHAT_H
+#define CHAT_H
 
-#define byte unsigned char
 #define BUFFER_SIZE 1024
-
-#define SERVER_path "server"
+#define SERVER_PATH "server"
 #define CLIENT_MAX_NAME_LEN 64
 
-
-typedef struct Client
+typedef struct
 {
-    int pipe_fd[2];
-} Client;
+    int client_id;
+    char fifo_name[CLIENT_MAX_NAME_LEN];
+} ClientInfo;
 
 void start_server();
 void start_client();
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include "chat_client.h"
-#include "chat_server.h"
+#include "../include/chat_client.h"
+#include "../include/chat_server.h"
 
 int main(int argc, char *argv[])
 {
